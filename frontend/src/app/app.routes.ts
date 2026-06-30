ea9563bc816a54e134d9ae2e668e3921ac723683
Lucas <luhrt123@gmail.com>
2026-06-30 16:27:58 -0300

    correção: / agora leva para /home

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -11,6 +11,7 @@ import { Cart } from './pages/cart/cart'
 
 
 export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
     {
         path: 'home',
         component: HomeComponent
